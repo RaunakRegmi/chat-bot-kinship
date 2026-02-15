c77b071d1dcd1d8173a7c6723007ac599f03b171
Raunak Regmi <raunak.regmi@gmail.com>
2026-02-15 20:53:33 +0545

    Changes

--- gemini_chatbot.py
+++ gemini_chatbot.py
@@ -1,29 +1,21 @@
 from google import genai
 import os
 
-client = genai.Client(api_key=os.getenv("AIzaSyANSib30IU8vkWsqOIJQHuHRiw2qwOwvtI"))
+client = genai.Client(
+    api_key="AIzaSyB_Xn5rGszohpzfz6_q5jaNXnRRCOc3O38"
+)
 
-system_instruction = """
-You are a friendly Nepali IT teacher.
-Answer clearly and shortly.
-"""
-
-print("Few-Shot Gemini Chatbot Started (type 'exit' to stop)")
-
-history = []
+print("Gemini Chatbot Started (type 'exit' to stop)")
 
 while True:
-    user_input = input("Student: ")
+    user_input = input("You: ")
 
     if user_input.lower() == "exit":
-        print("Chat ended.")
         break
 
-    history.append({"role": "user", "parts": user_input})
-
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
-        contents=system_instruction + "\n" + user_input
+        model="gemini-2.5-flash",
+        contents=user_input
     )
 
-    print("Teacher:", response.text)
+    print("Gemini:", response.text)
